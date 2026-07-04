8d855ff14e584592800c3460b9897cd9658c0f49
OM VERMA <omv71@gmail.com>
2026-07-04 17:15:50 +0530

    Fix dependency conflict

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["django-portfolio-xxxx.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://django-portfolio-xxxx.onrender.com"]
 
 
 # Application definition
