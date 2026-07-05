c5d85751f7bccaffb3fb416cc7ff87df7d74d1ee
OM VERMA <omv71@gmail.com>
2026-07-05 14:30:24 +0530

    Restore SMTP email backend

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 # ---------------- EMAIL CONFIG ---------------- #
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 
