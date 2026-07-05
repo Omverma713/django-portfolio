11986135f13ad5d7ce357703597940f28c243975
OM VERMA <omv71@gmail.com>
2026-07-05 13:44:15 +0530

    Restore project and fix hero responsiveness

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["django-portfolio-i2t8.onrender.com"]
+ALLOWED_HOSTS = ["django-portfolio-i2t8.onrender.com","*"]
 CSRF_TRUSTED_ORIGINS = ["https://django-portfolio-i2t8.onrender.com"]
 
 
